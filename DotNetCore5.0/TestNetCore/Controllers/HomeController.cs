0f483b65b1826ac4fcb98b2522fa6e90f2e5a8f1
ADMIN\Admin <vukhai27091998@gmail.com>
2022-01-12 00:17:38 +0700

    TestNetCore

---
+++ DotNetCore5.0/TestNetCore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestNetCore.Models;
+
+namespace TestNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        { 
+           
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetCore5.0/TestNetCore/Controllers/NewsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestNetCore.Controllers
+{
+    public class NewsController : Controller //ControllerBase
+    {
+        /*
+        // GET: NewsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NewsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StringOut2()
+        {
+            return new OkObjectResult("Hello Everyone Welcome to Chelsea FC");
+        }
+
+        public String StringOut(int id, Employee employee)
+        {
+            return ($"Hello Everyone Welcome to Chelsea FC FanName {id} : {employee.FirstName} {employee.LastName}");
+        }
+        public IActionResult JsonOut(int id, Employee employee)
+        {
+            var obj = new { Id = id, Employee = employee };
+            return Json(obj);
+        }
+        public class Employee {
+            public string FirstName { set; get; }
+            public string LastName { set; get; }
+        }
+    }
+}
