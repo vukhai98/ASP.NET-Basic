7c4464e12337a9a698c1cd7043cfc354b56c252b
ADMIN\Admin <vukhai27091998@gmail.com>
2022-01-17 11:29:39 +0700

    Syntax Basic in ASP.NET Razor

---
+++ DotNetCore5.0/ASP.NET08/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET08
+{
+    public class IndexModel : PageModel
+    {
+        public string Content { set; get; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            this.Content = " This is content in Content";
+
+        }
+    }
+}
