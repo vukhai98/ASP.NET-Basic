3f003b6f13285eabac9aed8105bd8df72cb73f30
ADMIN\Admin <vukhai27091998@gmail.com>
2022-01-18 12:28:34 +0700

    Review of Razor PageModel ASP.NET

---
+++ DotNetCore5.0/PageModelExample/Pages/ContactRequest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageModelExample
+{
+    public class ContactRequest : PageModel
+    {
+        public string UserId { set; get; } = "Id User.....";
+
+        private readonly ILogger<ContactRequest> logger;
+        public ContactRequest(ILogger<ContactRequest> _logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("Init contact........");
+        }
+
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
