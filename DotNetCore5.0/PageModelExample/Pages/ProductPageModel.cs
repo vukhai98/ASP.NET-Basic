9457d4ef9f675bd8469252dba5fe84b23daf2708
ADMIN\Admin <vukhai27091998@gmail.com>
2022-01-18 12:29:49 +0700

    Review of Razor PageModel ASP.NET

---
+++ DotNetCore5.0/PageModelExample/Pages/ProductPageModel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageModelExample
+{
+    public class ProductPageModel : PageModel
+    {
+        public readonly ProductService productService;
+        private readonly ILogger<PageModel> _logger;
+        public ProductPageModel(ILogger<ProductPageModel> logger, ProductService _productService)
+        {
+
+            _logger = logger;
+            productService = _productService;
+        }
+        public Product product { set; get; }
+        // OnGet, OnPost , OnGetAbc.... HttpRequest
+        // void , IActionResult
+        public void OnGet(int? id)
+        {
+            if (id != null)
+            {
+                ViewData["Title"] = $"Sản phẩm (ID = {id.Value})";
+                product = productService.Find(id.Value);
+            }
+            else
+            {
+                ViewData["Title"] = "Vi du trang sản phẩm";
+            }
+        }
+        // /product/{id:int?}?handler=lastproduct
+        public IActionResult OnGetLastProduct()
+        {
+            ViewData["Title"] = $"Sản phẩm cuối";
+            product = productService.AllProduct().LastOrDefault();
+            if (product != null)
+            {
+                return Page();
+            }
+            else
+            {
+                return this.Content("NotFound Product");
+            }
+        }
+
+        public IActionResult OnGetRemoveAll()
+        {
+            productService.AllProduct().Clear();
+            return RedirectToPage("ProductPageModel");
+        }
+        public IActionResult OnGetLoad()
+        {
+            productService.LoadProducts();
+            return RedirectToPage("ProductPageModel");
+        }
+        public IActionResult OnPostDelete(int? id)
+        {
+            if (id != null)
+            {
+                product = productService.Find(id.Value);
+                if (product != null)
+                {
+                    productService.AllProduct().Remove(product);
+                }
+            }
+            return this.RedirectToPage("ProductPageModel", new { id = string.Empty });
+        }
+    }
+    
+}
---
+++ DotNetCore5.0/PageModelExample/Services/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageModelExample
+{
+    public class ProductService
+    {
+        private List<Product> products = new List<Product>();
+        public ProductService()
+        {
+            LoadProducts();
+        }
+        public Product Find(int id)
+        {
+            var result = from p in products
+                         where p.Id == id
+                         select p;
+            return result.FirstOrDefault();
+        }
+        public List<Product> AllProduct() => products;
+        public void LoadProducts()
+        {
+            products.Clear();
+            products.Add(
+                new Product { Id = 1, Name = "Iphone", Description = " Điện thoại của Apple", Price = 1000 }
+              );
+            products.Add(
+               new Product { Id = 2, Name = "Samsung", Description = " Điện thoại của Samsung", Price = 900 }
+               );
+            products.Add(
+              new Product { Id = 3, Name = "Oppo", Description = " Điện thoại của Oppo", Price = 800 }
+              );
+        }
+    }
+}
